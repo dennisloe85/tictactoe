117bef5c879caf5ebdae4e805c378d15dc3a17ce
Dennis <dennis@loeben.de>
2020-08-20 21:14:19 +0200

    Changed from string to enum for board definition

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -6,50 +6,59 @@
 
 """
 
+from enum import Enum, auto
+
 class TicTacToe:
 
 
     board = []
 
-    player_label = ["X", "O"]
+    class BoardElem(Enum):
+        X       = auto()
+        O       = auto()
+        EMPTY   = auto()
 
     def _init(self):
-        self.board = [[" ", " ", " "],
-                      [" ", " ", " "],
-                      [" ", " ", " "]]
+
+
+        empty = self.BoardElem.EMPTY
+
+        self.board = [[empty, empty, empty],
+                      [empty, empty, empty],
+                      [empty, empty, empty]]
 
     def _show_board(cls, board):
         print() 
         print("      A     B     C\n")
 
         for idx, row in enumerate(board):
-            print(" " + str(idx + 1) + "    " + '  |  '.join(str(x) for x in row))
+            print(" " + str(idx + 1) + "    " + '  |  '.join(" " if x == cls.BoardElem.EMPTY else x.name for x in row))
             if idx != len(board) - 1: print("    -----------------")
         print() 
         print() 
 
     def _are_three_equal(cls, a, b, c):
-        return a == b and b == c and c != " "
+        return a == b and b == c
 
     def _get_winner(cls, board):
-        won_player = -1
+        won_player = cls.BoardElem.EMPTY
 
         # Check rows and columns
         for x in range(len(board)):
             # Check columns
-            if(cls._are_three_equal(board[0][x], board[1][x], board[2][x])):
+            if cls._are_three_equal(board[0][x], board[1][x], board[2][x]) and board[0][x] != cls.BoardElem.EMPTY:
                 won_player = board[0][x]
                 break
 
             # Check row
-            if(cls._are_three_equal(board[x][0], board[x][1], board[x][2])):
+            if cls._are_three_equal(board[x][0], board[x][1], board[x][2]) and board[x][0] != cls.BoardElem.EMPTY:
                 won_player = board[x][0]
                 break
 
         # Check diagonals if no winner found
-        if won_player == -1 and cls._are_three_equal(board[0][0], board[1][1], board[2][2]):
+        if won_player == cls.BoardElem.EMPTY and cls._are_three_equal(board[0][0], board[1][1], board[2][2]) and board[0][0] != cls.BoardElem.EMPTY:
             won_player = board[0][0]
-        if won_player == -1 and cls._are_three_equal(board[0][2], board[1][1], board[2][0]):
+        if won_player == cls.BoardElem.EMPTY and cls._are_three_equal(board[0][2], board[1][1], board[2][0]) and board[0][2] != cls.BoardElem.EMPTY:
             won_player = board[0][2]       
 
         return won_player
@@ -57,7 +66,7 @@ class TicTacToe:
     def run(self):
         self._init()
 
-        next_player = 0
+        next_player = self.BoardElem.X
 
         print()
         print("############ TicTacToe ############")
@@ -71,13 +80,13 @@ class TicTacToe:
 
             # Some won the game?
             tmp_winner = self._get_winner(self.board)
-            if tmp_winner != -1:
+            if tmp_winner != self.BoardElem.EMPTY:
                 print()
-                print(tmp_winner + " won the game!")
+                print(tmp_winner.name + " won the game!")
                 print()
                 run_game = False
                 break
-            elif sum( x.count(" ") for x in self.board) == 0:
+            elif sum( x.count(self.BoardElem.EMPTY) for x in self.board) == 0:
                 print()
                 print("Tie!")
                 print()
@@ -88,7 +97,7 @@ class TicTacToe:
             next_pos_user = []
             while(next_pos_user == []):
 
-                input_pos = input("It's " + self.player_label[next_player] + "'s turn. Please select your next position (e.g. A2): ")     
+                input_pos = input("It's " + next_player.name + "'s turn. Please select your next position (e.g. A2): ")     
                 print()      
 
                 # Remove whitespaces, tabs, new lines etc.
@@ -109,17 +118,17 @@ class TicTacToe:
 
                 # Get indices
                 next_pos_user_tmp = [ ord(input_pos[0]) - ord("A") , ord(input_pos[1]) - ord("1")]
-                if self.board[ next_pos_user_tmp[1]][next_pos_user_tmp[0]] != " ":
+                if self.board[ next_pos_user_tmp[1]][next_pos_user_tmp[0]] != self.BoardElem.EMPTY:
                     print("Position is already occupied!")
                     continue                     
 
                 next_pos_user = next_pos_user_tmp
              
             # Set player's choice
-            self.board[ next_pos_user_tmp[1]][next_pos_user_tmp[0]] = self.player_label[next_player]
+            self.board[ next_pos_user_tmp[1]][next_pos_user_tmp[0]] = next_player
 
             # Who's next?
-            next_player = ( next_player + 1 ) % 2
+            next_player = self.BoardElem.X if next_player == self.BoardElem.O else self.BoardElem.O
 
 
 
